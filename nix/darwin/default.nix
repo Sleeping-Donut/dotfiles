cdfb653eb4c0fd1a7b517c4a22b212e3551aa49b
Sleeping-Donut <nathandavis1999@gmail.com>
2024-01-24 05:41:39 +0000

    fix: syntax errors in darwin nix configs

--- nix/darwin/default.nix
+++ nix/darwin/default.nix
@@ -35,12 +35,12 @@ in
 
 	X68000 = darwin.lib.darwinSystem {					# MacBook12,1 (Early 2015) "Core i5" 2.7Ghz 8GB 2560x1600
 		inherit system;
-		specialArgs = { inherit user inputs nixpkgs nix-homebrew nur; hostname = "X68000"; system = "x86_64-darwin"; }
+		specialArgs = { inherit user inputs nixpkgs nix-homebrew nur; hostname = "X68000"; system = "x86_64-darwin"; };
 		modules = [
 			./configuration.nix {						# configs for darwin, home-manager setup networking etc.
 				homebrew.brews = [ ];
 				homebrew.casks = [ "firefox" "1password" ];
-				homebrew.masApps = { WireGuard = 1451685025 };
+				homebrew.masApps = { WireGuard = 1451685025; };
 			}
 
 			nix-homebrew.darwinModules.nix-homebrew { nix-homebrew = nix-homebrew-config; }
@@ -61,12 +61,12 @@ in
 	
 	LHC = darwin.lib.darwinSystem {					# Mac details go here
 		inherit system;
-		specialArgs = { inherit user inputs nixpkgs nix-homebrew nur; hostname = "LHC"; system = "aarch64-darwin" };
+		specialArgs = { inherit user inputs nixpkgs nix-homebrew nur; hostname = "LHC"; system = "aarch64-darwin"; };
 		modules = [
 			./configuration.nix {						# configs for darwin, home-manager setup networking etc.
 				homebrew.brews = [ ];
 				homebrew.casks = [ "firefox" "1password" ];
-				homebrew.masApps = { WireGuard = 1451685025 };
+				homebrew.masApps = { WireGuard = 1451685025; };
 			}
 
 			nix-homebrew.darwinModules.nix-homebrew { nix-homebrew = nix-homebrew-config; }
