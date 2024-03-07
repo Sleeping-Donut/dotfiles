5429d1d55386718e4795fb933fa1efd43dac7bf7
Sleeping-Donut <nathandavis1999@gmail.com>
2024-03-07 17:52:31 +0000

    do proper checks for deps on TS grammer gen

--- config/nvim/lua/nd0/lite/plugins/treesitter.lua
+++ config/nvim/lua/nd0/lite/plugins/treesitter.lua
@@ -15,10 +15,12 @@ return {
 		local configs = require("nvim-treesitter.configs")
 		local sitters = {}
 		
-		for _, v in ipairs({"lua", "vim", "vimdoc", "luadoc",
+		for _, v in ipairs({
+			"lua", "vim", "vimdoc", "luadoc",
 			"html", "javascript", "css",
-			"json", "jsonc", "scss", "tsx", "astro", "vue", "svelte",
-			"markdown", "comment",
+			"json", "jsonc", "scss", "tsx", "vue", "svelte",
+			-- Add in asciidoc once someone gets around to making the parser
+			"markdown", "comment", -- "asciidoc",
 			"python",
 			"bash", "fish",
 			"c", "cpp", "cmake",
@@ -44,10 +46,26 @@ return {
 		}) do
 			table.insert(sitters, v)
 		end
-		if vim.fn.executable("node") == 1 then
-			for _, v in ipairs({"swift"}) do
-				table.insert(sitters, v)
-			end
+
+		-- These require tree-sitter cli to generate parsers
+		if vim.fn.executable("tree-sitter") == 1 then
+			local node_sitters = {
+				"swift"
+			}
+			local c_sitters = {
+				"astro"
+			}
+
+			if vim.fn.executable("node") == 1 then
+				for _, v in ipairs(node_sitters) do
+					table.insert(sitters, v)
+				end
+			else print("err: not all TS installed dep node not found") end
+			if 1 <= vim.fn.executable("gcc") + vim.fn.executable("clang") then
+				for _, v in ipairs(c_sitters) do
+					table.insert(sitters, v)
+				end
+			else print("err: not all TS installed dep node not found") end
 		end
 			
 		configs.setup({
