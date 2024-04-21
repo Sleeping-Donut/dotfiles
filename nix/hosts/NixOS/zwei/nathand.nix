c1920a2f19ce7c951c4690574689b8b8d2d3a27e
Sleeping-Donut <nathandavis1999@gmail.com>
2024-04-21 03:35:33 +0100

    fix typo in zwei nathand groups

--- nix/hosts/NixOS/zwei/nathand.nix
+++ nix/hosts/NixOS/zwei/nathand.nix
@@ -12,7 +12,7 @@ in
 #	- uses default shell
 	isNormalUser = true;
 	description = "Nathan";
-	extraGroups = [ "wheel" "networkmanager" "labemembers" ];
+	extraGroups = [ "wheel" "networkmanager" "labmembers" ];
 	openssh.authorizedKeys.keys = [
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG2rAuYj5hGLj6eFScSjJoz5XXZzTiQVPWdL+fWUtp9q" # LHC
 	];
