5000853bca48d49dc4ca7903de6ca29f19f5a9a9
Sleeping-Donut <nathandavis1999@gmail.com>
2024-04-21 03:13:18 +0100

    update zwei

--- nix/hosts/NixOS/zwei/default.nix
+++ nix/hosts/NixOS/zwei/default.nix
@@ -27,13 +27,6 @@ in
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
-	fileSystems = {
-		"/".options = [ "compress=zstd" ];
-		"/home".options = [ "compress=zstd" ];
-		"/nix".options = [ "compress=zstd" "noatime" ];
-		"/swap".options = [ "noatime" ];
-	};
-	swapDevices = [];
 	fileSystems."/mnt/amadeus/fg8" = {
 		device = "whitefox.fglab:/mnt/amadeus/fg8";
 		fsType = "nfs";
@@ -47,10 +40,20 @@ in
 	time.timeZone = "GB";
 
 	i18n.defaultLocale = "en_GB.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_GB.UTF-8";
+		LC_IDENTIFICATION = "en_GB.UTF-8";
+		LC_MEASUREMENT = "en_GB.UTF-8";
+		LC_MONETARY = "en_GB.UTF-8";
+		LC_NAME = "en_GB.UTF-8";
+		LC_NUMERIC = "en_GB.UTF-8";
+		LC_PAPER = "en_GB.UTF-8";
+		LC_TELEPHONE = "en_GB.UTF-8";
+		LC_TIME = "en_GB.UTF-8";
+	};
 	console = {
 		font = "Lat2-Terminus16";
 		keyMap = "uk";
-		useXkbConfig = true; # for xkb.options in tty
 	};
 
 #	To enable sound
@@ -71,8 +74,8 @@ in
 		enable = true;
 		group = "labmembers";
 		dataDir = "/opt/plex/data";
-		extraScanners = "/opt/plex/scanners";
-		extraPlugins = "/opt/plex/plugins";
+		extraScanners = [];
+		extraPlugins = [];
 		openFirewall = true;
 		package = pkgs-unstable.plex;
 	};
