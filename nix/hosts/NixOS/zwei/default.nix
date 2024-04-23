3da0294001e5cee8751ea57d8f2d0f46bfe811e3
Sleeping-Donut <nathandavis1999@gmail.com>
2024-04-23 02:28:46 +0100

    fix: use correct method to override plex pkg for zwei

--- nix/hosts/NixOS/zwei/default.nix
+++ nix/hosts/NixOS/zwei/default.nix
@@ -6,10 +6,16 @@
 	...
 }:
 let
-	plex-versioned = ({version, hash}:
-		pkgs.fetchurl {
-			url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-			hash = hash;
+	plex-versioned = (args@{version, hash}:
+		pkgs.plex.override {
+			plexRaw = pkgs.plexRaw.overrideAttrs(old: rec {
+				inherit version;
+				src = pkgs.fetchurl {
+					name = "plex-${version}";
+					url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
+					hash = hash;
+				};
+			});
 		}
 	);
 in
