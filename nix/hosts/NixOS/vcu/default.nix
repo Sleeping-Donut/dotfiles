75e1768dfb7d3873965af76282c6e8ef1760db6f
Sleeping-Donut <nathandavis1999@gmail.com>
2024-09-12 02:08:50 +0100

    Revert "fixup! fixup! fixup! add prowlarr to vcu"
    This reverts commit e080687d73e1381da8e4c0c6daee01c19748be4b.

--- nix/hosts/NixOS/vcu/default.nix
+++ nix/hosts/NixOS/vcu/default.nix
@@ -131,7 +131,7 @@ in
 #	NOTE: required otherwise mullvad cant resolve DNS correctly
 	services.resolved.enable = true;
 
-	services.nd0-prowlarr = {
+	nd0.services.prowlarr = {
 		enable = true;
 		group = "labmembers";
 		dataDir = "/opt/prowlarr/data";
