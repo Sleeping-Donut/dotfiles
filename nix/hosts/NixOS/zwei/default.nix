f954cb9179fe12e40074262c303d9b95c4ccb320
Sleeping-Donut <nathandavis1999@gmail.com>
2025-03-01 02:09:19 +0000

    fixup! fix: use newer unifi mongo to avoid EOL

--- nix/hosts/NixOS/zwei/default.nix
+++ nix/hosts/NixOS/zwei/default.nix
@@ -113,8 +113,8 @@ in
 		enable = true;
 		maximumJavaHeapSize = 2048;
 		openFirewall = true;
-		unifiPackage = unstable.unifi;
-		mongodbPackage = unstable.mongodb-7_0;
+		unifiPackage = pkgs-unstable.unifi;
+		mongodbPackage = pkgs-unstable.mongodb-7_0;
 		# Files have to be in `/var/lib/unifi` (╥‸╥)
 	};
 
