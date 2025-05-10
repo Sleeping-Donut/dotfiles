7a1d3127bdefe984fc2329bca683858c71ec4cc6
Sleeping-Donut <nathandavis1999@gmail.com>
2025-05-10 18:47:08 +0100

    remove redundant nginx config options

--- nix/hosts/NixOS/zwei/default.nix
+++ nix/hosts/NixOS/zwei/default.nix
@@ -276,10 +276,6 @@ in
 						proxy_set_header X-Forwarded-Host $host;
 						proxy_set_header X-Forwarded-Proto $scheme;
 						proxy_redirect off;
-						proxy_http_version 1.1;
-
-						proxy_set_header Upgrade $http_upgrade;
-						proxy_set_header Connection $http_connection;
 
 					'';
 				};
