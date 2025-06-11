93b516b99e79c6fe20bec8e57c03a8ee2fdfb155
Sleeping-Donut <nathandavis1999@gmail.com>
2025-06-12 00:48:49 +0100

    fixup! update plex zwei

--- nix/overrides/plex-versioned.nix
+++ nix/overrides/plex-versioned.nix
@@ -1,14 +1,12 @@
 { pkgs }:
 (args@{version, hash}:
-	pkgs.plex.override {
-		plexRaw = pkgs.plexRaw.overrideAttrs(old: rec {
-			inherit version;
-			src = pkgs.fetchurl {
-				name = "plex-${version}";
-				url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-				hash = hash;
-			};
-		});
-	}
+	pkgs.plex.overrideAttrs (old: rec {
+		inherit version;
+		src = pkgs.fetchurl {
+			name = "plex-${version}";
+			url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
+			hash = hash;
+		};
+	})
 )
 
