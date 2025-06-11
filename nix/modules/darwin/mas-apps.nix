89182d2737f101f1d2508f7fb73e96c1096cec0b
Sleeping-Donut <nathandavis1999@gmail.com>
2025-06-11 21:13:23 +0100

    add some macos app store apps

--- nix/modules/darwin/mas-apps.nix
+++ nix/modules/darwin/mas-apps.nix
@@ -5,7 +5,9 @@
 	MicrosoftRemoteDesktop = 1295203466;
 	Paperback = 1626613373;
 	Tachimanga = 6447486175;
+	Infuse = 1136220934;
 	Toggl = 1291898086;
 	Twitter = 1482454543;
 	WireGuard = 1451685025;
+	XCode = 497799835;
 }
