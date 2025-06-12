b141e0ede0c2aa6a45a589db4cc98c8c03b4a3c2
Sleeping-Donut <nathandavis1999@gmail.com>
2025-06-12 01:43:37 +0100

    update versioned plex code

--- nix/overrides/plex-versioned.nix
+++ nix/overrides/plex-versioned.nix
@@ -1,12 +1,18 @@
 { pkgs }:
-(args@{version, hash}:
-	pkgs.plex.overrideAttrs (old: rec {
+( args@{ version, hash }: let
+
+	arch = if pkgs.stdenv.hostPlatform.system == "aarch64-linux" then "arm64"
+		else if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then "amd64"
+		else "armhf"; # Should never reach here because support so low
+
+	url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_${arch}.deb";
+
+	plexVersionedRaw = pkgs.plexRaw.overrideAttrs (old: {
 		inherit version;
-		src = pkgs.fetchurl {
-			name = "plex-${version}";
-			url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-			hash = hash;
-		};
-	})
+		name = "${old.pname}-${version}";
+		src = pkgs.fetchurl { inherit url hash;};
+	});
+in
+	pkgs.plex.override { plexRaw = plexVersionedRaw; }
 )
 
