5db55f8c36d34679698926283dfdaff663f498a7
Sleeping-Donut <nathandavis1999@gmail.com>
2025-09-23 18:27:23 +0100

    add nixos-rebuild script to allow running in the background
    use tmux to run rebuilds in the background
    use watch and tmux capture-pane to show the user the current status
    use nix-output-monitor for formatting

--- nix/hosts/NixOS/zwei/nathand.nix
+++ nix/hosts/NixOS/zwei/nathand.nix
@@ -10,6 +10,34 @@
 	home.file.".profile".text = ''
 		export PATH="$PATH:$HOME/.local/bin"
 	'';
+	home.file.".local/bin/bgrebuild" = let
+		bgrebuild-script = pkgs.writeShellScript "bgrebuild" ''
+			#!${pkgs.stdenv.shell}
+
+			tmux_cmd="${pkgs.lib.getExe pkgs.tmux}"
+			watch_cmd="${pkgs.lib.getExe pkgs.watch}"
+			nom_cmd="${pkgs.lib.getExe pkgs.nix-output-monitor}"
+
+			if $tmux_cmd has-session -t nixos-rebuild 2>/dev/null; then
+				echo "Attaching to existing 'nixos-rebuild' session..."
+				$tmux_cmd attach-session -t nixos-rebuild
+				echo "Error: a 'nixos-rebuild' session already exists. Use 'tmux attach -t nixos-rebuild' to view it" >&2
+				exit 1
+			fi
+
+			# Use the provided argument as the flake path, or default to the current directory
+			flake_path="$${1:-.}"
+
+			echo "Starting a new 'nixos-rebuild' session..."
+			sudo $tmux_cmd new-session -d -s nixos-rebuild "$nom_cmd nixos-rebuild switch --flake $flake_path"
+
+			# Watch the output with color and capture scrollback
+			$watch_cmd -c "$tmux_cmd capture-pane -t nixos-build -pS-"
+		'';
+	in {
+		executable = true;
+		source = bgrebuild-script;
+	};
 
 	home.packages = with pkgs-unstable; [
 		bat
