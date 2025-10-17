23a68a1a1cb5f874dc8784e037d84c3285cbeabc
Sleeping-Donut <nathandavis1999@gmail.com>
2025-10-17 15:24:55 +0100

    add htpc config

---
+++ nix/hosts/NixOS/htpc/default.nix
@@ -0,0 +1,141 @@
+{
+	config, lib, pkgs,
+	pkgs-unstable,
+	nix-modules, nixos-modules, overrides, own-pkgs,
+	repo-root,
+	hostname ? "htpc", system,
+	inputs,
+	...
+}:
+let
+	keys = import nix-modules.keys;
+in
+{
+	imports = [
+		./hardware-configuration.nix
+	];
+
+#	This defines first version of nixos installed - used to maintain
+#	 compatibility with application data (e.g. databases)
+#	Most users should NEVER update this value even when updating nixos
+#	Do NOT change this value unless you have manually inspected all changes it
+#	 would make to your configuration and migrated your data accordingly
+	system.stateVersion = "23.11"; # Did you read the comment?
+
+	nix = {
+		gc.automatic = true;
+		gc.dates = "weekly";
+		gc.options = "--delete-older-than 30d";
+		optimise.automatic = true;
+	};
+
+	networking = {
+		hostName = hostname;
+		networkmanager.enable = true;
+		firewall = {
+			enable = true;
+		};
+		extraHosts = ''
+			127.0.0.1		zwei.fglab
+			192.168.10.124	whitefox.fglab
+			192.168.10.117	vcu.fglab
+		'';
+	};
+
+#	Use systemd-boot EFI bootloader
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.systemd-boot.configurationLimit = 8;
+	boot.loader.efi.canTouchEfiVariables = true;
+
+	fileSystems."/mnt/amadeus/fg8" = {
+		device = "whitefox.fglab:/mnt/amadeus/fg8";
+		fsType = "nfs";
+		options = [
+			"nfsvers=4.2"
+			"x-systemd.automount" # noauto | automount
+			"x-systemd.idle-timeout=1200" # disconnects after 20 minutes (i.e. 1200 seconds)
+		];
+	};
+
+	time.timeZone = "GB";
+
+	i18n.defaultLocale = "en_GB.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_GB.UTF-8";
+		LC_IDENTIFICATION = "en_GB.UTF-8";
+		LC_MEASUREMENT = "en_GB.UTF-8";
+		LC_MONETARY = "en_GB.UTF-8";
+		LC_NAME = "en_GB.UTF-8";
+		LC_NUMERIC = "en_GB.UTF-8";
+		LC_PAPER = "en_GB.UTF-8";
+		LC_TELEPHONE = "en_GB.UTF-8";
+		LC_TIME = "en_GB.UTF-8";
+	};
+	console = {
+		font = "Lat2-Terminus16";
+		keyMap = "uk";
+	};
+
+	xdg.portal = {
+		enable = true;
+		xdgOpenUsePortal = true;
+		extraPortals = with pkgs; [
+			kdePackages.xdg-desktop-portal-kde
+		];
+	};
+
+	services.xserver.enable = true;
+	services.displayManager.sddm = {
+		enable = true;
+		wayland.enable = true;
+	};
+	services.desktopManager.plasma6.enable = true;
+
+#	Groups
+	users.groups.labmembers.gid = 8596;
+
+	users.users.nathand = {
+		isNormalUser = true;
+		description = "Nathan";
+		extraGroups = [ "wheel" "networkmanager" "labmembers" ];
+		openssh.authorizedKeys.keys = [
+			keys.LHC
+			keys.s24u
+		];
+	};
+
+#	Home Configs
+	home-manager = {
+		users.nathand = import ./nathand.nix;
+	};
+
+
+#	System packages
+	environment.systemPackages = with pkgs-unstable; [
+		curl
+		dua
+		duf
+		dust
+		fd
+		git
+		neovim
+		nh
+		nix-output-monitor
+		plex-desktop
+		wget
+	];
+	programs.tmux = {
+		enable = true;
+		baseIndex = 1;
+		keyMode = "vi";
+		extraConfig = "set-option -g prefix2 C-'\\'";
+	};
+
+#	System Services
+	services.openssh.enable = true;
+	services.tailscale = {
+		enable = true;
+		package = pkgs-unstable.tailscale;
+	};
+}
+
---
+++ nix/hosts/NixOS/htpc/nathand.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  config,
+  pkgs,
+  pkgs-unstable,
+  repo-root,
+  ...
+}:
+{
+  home.stateVersion = "23.11";
+
+  home.file.".profile".text = ''
+    		export PATH="$PATH:$HOME/.local/bin"
+    	'';
+
+  home.packages = with pkgs-unstable; [
+    bat
+    btop
+    ripgrep
+    speedtest-go
+    systemctl-tui
+    traceroute
+  ];
+}
