adaabff199bcbd15dfd4f78ae003669cf6d11d62
Sleeping-Donut <nathandavis1999@gmail.com>
2025-10-22 19:42:19 +0100

    remove typoed unfree firmware blobs

--- nix/hosts/NixOS/htpc/hardware-configuration.nix
+++ nix/hosts/NixOS/htpc/hardware-configuration.nix
@@ -89,7 +89,6 @@
   # networking.interfaces.eno4.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.enableAllFirmare = true;
   hardware.enableRedistributableFirmware = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
