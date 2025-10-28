bf40e267956f62b67fd71eebfd5c62d55d7ccfa7
Sleeping-Donut <nathandavis1999@gmail.com>
2025-10-28 23:11:18 +0000

    add new nix-on0droid config

---
+++ nix/hosts/android/s24u.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  pkgs-unstable
+}:
+{
+  system.stateVersion = "24.05";
+
+  time.timeZone = "Europe/London";
+
+  environment.etcBackupExtension = ".bak";
+  environment.packages = with pkgs-unstable; [
+    curl
+    fd
+    git
+    neovim
+    ripgrep
+    wget
+  ];
+}
