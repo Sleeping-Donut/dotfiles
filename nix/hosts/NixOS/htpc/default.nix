2622e0f86f6caedcc86d7699b58565b459de0118
Sleeping-Donut <nathandavis1999@gmail.com>
2026-01-17 04:30:21 +0000

    add plymouth config
    multi-colored sun looking boot logo

--- nix/hosts/NixOS/htpc/default.nix
+++ nix/hosts/NixOS/htpc/default.nix
@@ -78,6 +78,29 @@ in
 		keyMap = "uk";
 	};
 
+	boot.plymouth = {
+		enable = true;
+    theme = "colorful_sliced";
+    themePackages = with pkgs; [
+      (adi1090x-plymouth-themes.override {
+        selected_themes = ["colorful_sliced"];
+      })
+    ];
+	};
+	boot.consoleLogLevel = 3; # 'Silent' boot
+	boot.initrd.verbose = false;
+	boot.kernelParams = [
+		"quiet"
+		"splash"
+		"boot.shell_on_fail"
+		"udev.log_priority=3"
+		"rd.systemd.show_status=auto"
+	];
+	# Hide the OS choice for bootloaders.
+	# It's still possible to open the bootloader list by pressing any key
+	# It will just not appear on screen unless a key is pressed
+	boot.loader.timeout = 0;
+
 	xdg.portal = {
 		enable = true;
 		xdgOpenUsePortal = true;
