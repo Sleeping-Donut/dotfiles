514034cd67142fc2c918ae7f126ce34355577d52
Sleeping-Donut <nathandavis1999@gmail.com>
2026-06-04 02:41:26 +0100

    remove non-existent font option

--- nix/hosts/android/s24u.nix
+++ nix/hosts/android/s24u.nix
@@ -11,6 +11,14 @@
   ...
 }:
 {
+  imports = [
+    modules.home.neovim
+    modules.home.zsh
+    modules.home.shell-profile
+    modules.home.tealdeer
+    modules.home.tmux
+  ];
+
   system.stateVersion = "24.05";
 
   time.timeZone = "Europe/London";
@@ -20,17 +28,29 @@
     computerName = hostname;
   };
 
-  fonts.packages = with pkgs; [
-    noto-fonts
-  ];
-
   environment.etcBackupExtension = ".bak";
   environment.packages = with pkgs-unstable; [
     curl
     fd
     git
     neovim
+    noto-fonts
     ripgrep
     wget
   ];
+
+  nd0.home = {
+    neovim = {
+      enable = true;
+      lsps = false;
+      formatters = false;
+    };
+    shell-profile = {
+      enable = true;
+      symlink.enable = true;
+    };
+    tealdeer.enable = true;
+    tmux.enable = true;
+    zsh.enable = true;
+  };
 }
