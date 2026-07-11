d4c5f3f73367916a7388f116e92ccc63ad08c2b4
Sleeping-Donut <nathandavis1999@gmail.com>
2026-07-11 14:54:19 +0100

    add pocket-id for auth

--- nix/hosts/NixOS/zwei/default.nix
+++ nix/hosts/NixOS/zwei/default.nix
@@ -323,6 +323,30 @@ in
     # Files have to be in `/var/lib/unifi` (╥‸╥)
   };
 
+  users.users.pocket-id.extraGroups = [ "labmembers" ];
+  systemd.tmpfiles.settings.pocket-id = {
+    "/opt/pocket-id/data" = {
+      d = {
+        user = "pocket-id";
+        group = "pocket-id";
+        mode = "0700";
+      };
+    };
+  };
+  services.pocket-id = {
+    enable = true;
+    dataDir = "/opt/pocket-id/data";
+    settings = {
+      APP_URL = "https://id.${publicDomain}";
+      TRUST_PROXY = true;
+      HOST = "127.0.0.1";
+      PORT = 1411;
+    };
+    credentials = {
+      ENCRYPTION_KEY = "/opt/pocket-id/encryption-key";
+    };
+  };
+
   services.prometheus = {
     package = pkgs-unstable.prometheus;
 
@@ -562,6 +586,14 @@ in
         };
       };
 
+      virtualHosts."id.${publicDomain}" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = toUrl zwei 1411;
+          proxyWebsockets = true;
+        };
+      };
       virtualHosts."immich.${publicDomain}" = {
         enableACME = true;
         forceSSL = true;
