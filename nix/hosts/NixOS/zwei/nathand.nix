69f77219d545f10d9d2da8e7548ae90f2a26b298
Sleeping-Donut <nathandavis1999@gmail.com>
2026-08-13 17:36:15 +0100

    formatting
    nix can't parse tab whitespace well. leading spaces get removed in block
    text

--- nix/hosts/NixOS/zwei/nathand.nix
+++ nix/hosts/NixOS/zwei/nathand.nix
@@ -13,12 +13,12 @@
   home.stateVersion = "23.11";
 
   home.file.".profile".text = ''
-    		export PATH="$PATH:$HOME/.local/bin"
-            alias dotpull='cd ~/dotfiles \
-              && echo "Pulling ~/dotfiles" && git pull \
-              && cd - > /dev/null'
-            alias nixup='nh os switch -H zwei ~/dotfiles'
-    	'';
+    export PATH="$PATH:$HOME/.local/bin"
+    alias dotpull='cd ~/dotfiles \
+      && echo "Pulling ~/dotfiles" && git pull \
+      && cd - > /dev/null'
+    alias nixup='nh os switch -H zwei ~/dotfiles'
+  '';
 
   home.packages = with pkgs-unstable; [
     bat
