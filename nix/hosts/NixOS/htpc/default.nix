9da3a9d31289f2d0ff3dfd6fc924219fb6c40737
Sleeping-Donut <nathandavis1999@gmail.com>
2026-09-07 22:43:02 +0100

    formatting

--- nix/hosts/NixOS/htpc/default.nix
+++ nix/hosts/NixOS/htpc/default.nix
@@ -41,10 +41,10 @@ in
       enable = true;
     };
     extraHosts = ''
-      			127.0.0.1		zwei.fglab
-      			192.168.10.124	whitefox.fglab
-      			192.168.10.117	vcu.fglab
-      		'';
+      127.0.0.1       zwei.fglab
+      192.168.10.124  whitefox.fglab
+      192.168.10.117  vcu.fglab
+    '';
   };
 
   #	Use systemd-boot EFI bootloader
